d51365c02f2404365c40e0bc276e483729ba1ab9
Valentin VERMANDE <vvermande@arketeam.com>
2022-08-19 19:15:19 +0200

    test url navigate

--- src/App.tsx
+++ src/App.tsx
@@ -18,16 +18,13 @@ function App() {
 
     const {user} = useAuthContext();
 
-    const basePath = process.env.NODE_ENV === 'production' ?
-        '/pkdextracker/' : '/';
-
     return <div className="App">
         <div className="background"></div>
         <BrowserRouter>
             <Routes>
-                {GENERATIONS.map(g => <Route key={g} path={basePath+(g+1)} element={<PokeList genIndex={g}/>}/>)}
-                <Route path={basePath} element={user ? <Home/> : <Login/>}/>
-                <Route path={basePath + '*'} element={<Test/>}/>
+                {GENERATIONS.map(g => <Route key={g} path={'/'+(g+1)} element={<PokeList genIndex={g}/>}/>)}
+                <Route path={'/'} element={user ? <Home/> : <Login/>}/>
+                <Route path={'/*'} element={<Test/>}/>
             </Routes>
         </BrowserRouter>
     </div>
