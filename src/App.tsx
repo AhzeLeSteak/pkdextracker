1ef56a4eb9d559488f69bc6614887c376735d94f
Valentin VERMANDE <vvermande@arketeam.com>
2022-08-19 18:41:49 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
 import PokeList from "./pages/PokeList";
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes, useLocation} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import {useAuthContext} from "./firebase/AuthProvider";
 
 const GENERATIONS = Array.from({length: 8}, (_, i) => i);
+
+const Test = (props: any) => {
+    console.log(props);
+    const l = useLocation();
+    console.log(l);
+    return <></>;
+}
 
 function App() {
 
@@ -17,7 +24,7 @@ function App() {
             <Routes>
                 {GENERATIONS.map(g => <Route key={g} path={'/'+(g+1)} element={<PokeList genIndex={g}/>}/>)}
                 <Route path="/" element={user ? <Home/> : <Login/>}/>
-                <Route path="/*" element={<Navigate to="/"/>}/>
+                <Route path="/*" element={<Test/>}/>
             </Routes>
         </BrowserRouter>
     </div>
